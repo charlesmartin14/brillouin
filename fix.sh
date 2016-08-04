33e170cf3b7d78b3c1aa2402937c5c18bafdc0d3
Charles H Martin <charlesmartin14@gmail.com>
2016-08-04 16:48:13 -0700

    basic script to fix code formatting

--- fix.sh
+++ fix.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env  bash
-cat $1 | tr '∗' '*' | tr '−' '-' | sed -e 's/ d0/d0/g' | sed -e 's/ (/(/g' | sed -e 's/ )/)/g' | sed -e 's/( /(/g' | sed -e 's/ )/)/g' | sed -e 's/ ,/,/g' | sed -e 's/, /,/g' | sed -e 's/ ∗ ∗/**/g' | sed -e 's/∗ ∗/**/g'  | sed -e 's/ \. /./g' | sed -e 's/^ [0-9][0-9] /    /' | sed -e 's/^ [0-9][0-9][0-9]/    /'
+cat $1  | sed -e 's/ ∗ ∗/**/g' | sed -e 's/∗ ∗/**/g' | tr '∗' '*' | tr '−' '-' | sed -e 's/ d0/d0/g' | sed -e 's/ (/(/g' | sed -e 's/ )/)/g' | sed -e 's/( /(/g' | sed -e 's/ )/)/g' | sed -e 's/ ,/,/g' | sed -e 's/, /,/g'  | sed -e 's/ \. /./g' | sed -e 's/^ [0-9][0-9] /    /' | sed -e 's/^ [0-9][0-9][0-9]/    /' 
 
 
